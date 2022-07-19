fb7d8cf16f80a5e33fe47aa012152170c7a6fdb7
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-07-19 20:29:46 +0100

    CS

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -29,14 +29,12 @@ int _max(int *arr, size_t size)
  */
 void counting_sort(int *array, size_t size)
 {
-	int i, n, *output, *count, max;
+	int i, *output, *count, max;
 
 	output = malloc(sizeof(int) * size);
 	if (!output)
 		return;
-
 	max = _max(array, size);
-	n = (int)size;
 
 	count = malloc(sizeof(int) * (max + 1));
 	if (!count)
@@ -47,19 +45,19 @@ void counting_sort(int *array, size_t size)
 	for (i = 0; i <= max; ++i)
 		count[i] = 0;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < (int)size; i++)
 		count[array[i]]++;
 
 	for (i = 1; i <= max; i++)
 		count[i] += count[i - 1];
 	print_array(count, max + 1);
 
-	for (i = n - 1; i >= 0; i--)
+	for (i = size - 1; i >= 0; i--)
 	{
 		output[count[array[i]] - 1] = array[i];
 		count[array[i]]--;
 	}
-	for (i = 0; i < n; i++)
+	for (i = 0; i < (int)size; i++)
 		array[i] = output[i];
 	free(output);
 	free(count);
