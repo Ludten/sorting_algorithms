8eb2c08f2d8a21b81fe2b706d7df2e36629d9c57
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-07-19 16:49:27 +0100

    Knuth shell

---
+++ 100-shell_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j, k;
+	int temp;
+
+	gap = 1;
+	while (gap < size)
+	{
+		gap = gap * 3 + 1;
+	}
+	k = 0;
+	for (; gap > 0; gap /= 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+				array[j] = array[j - gap];
+
+			array[j] = temp;
+		}
+		if (k > 0)
+			print_array(array, size);
+		k++;
+	}
+}
